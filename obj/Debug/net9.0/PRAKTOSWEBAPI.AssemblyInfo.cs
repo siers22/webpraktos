6ef99618e179873b31d6438ea01f8658bb4c41fd
siers22 <siers6292@gmail.com>
2025-12-10 16:09:32 +0500

    some work

--- obj/Debug/net9.0/PRAKTOSWEBAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/PRAKTOSWEBAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRAKTOSWEBAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea6b493d7a5abb67d742dd5464411ce9fba902cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRAKTOSWEBAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRAKTOSWEBAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
